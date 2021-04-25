9703b3db07a4b2fc348f17d484d0de839cf2adb7
WasifBoomz <72422316+WasifBoomz@users.noreply.github.com>
2021-04-24 21:27:06 -0400

    Started It
    Yes

---
+++ Gemlik Patcher/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Gemlik_Patcher
+{
+    partial class GemlikPatcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GemlikPatcher));
+            this.Mods = new System.Windows.Forms.Button();
+            this.Settings = new System.Windows.Forms.Button();
+            this.Quit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Mods
+            // 
+            this.Mods.Font = new System.Drawing.Font("Eras Bold ITC", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Mods.Location = new System.Drawing.Point(260, 139);
+            this.Mods.Name = "Mods";
+            this.Mods.Size = new System.Drawing.Size(100, 72);
+            this.Mods.TabIndex = 0;
+            this.Mods.Text = "Mods";
+            this.Mods.UseVisualStyleBackColor = true;
+            // 
+            // Settings
+            // 
+            this.Settings.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Settings.Font = new System.Drawing.Font("Eras Bold ITC", 12F, System.Drawing.FontStyle.Bold);
+            this.Settings.Location = new System.Drawing.Point(249, 56);
+            this.Settings.Name = "Settings";
+            this.Settings.Size = new System.Drawing.Size(100, 30);
+            this.Settings.TabIndex = 1;
+            this.Settings.Text = "Settings";
+            this.Settings.UseVisualStyleBackColor = true;
+            this.Settings.Click += new System.EventHandler(this.Settings_Click);
+            // 
+            // Quit
+            // 
+            this.Quit.BackColor = System.Drawing.Color.Red;
+            this.Quit.Font = new System.Drawing.Font("Eras Bold ITC", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Quit.Location = new System.Drawing.Point(249, 237);
+            this.Quit.Name = "Quit";
+            this.Quit.Size = new System.Drawing.Size(100, 39);
+            this.Quit.TabIndex = 2;
+            this.Quit.Text = "Quit";
+            this.Quit.UseVisualStyleBackColor = false;
+            this.Quit.Click += new System.EventHandler(this.Quit_Click);
+            // 
+            // GemlikPatcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BackgroundImage = global::Gemlik_Patcher.Properties.Resources.Ratchet;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(624, 321);
+            this.Controls.Add(this.Quit);
+            this.Controls.Add(this.Settings);
+            this.Controls.Add(this.Mods);
+            this.DoubleBuffered = true;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GemlikPatcher";
+            this.Text = "Gemlik Patcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Mods;
+        private System.Windows.Forms.Button Settings;
+        private System.Windows.Forms.Button Quit;
+    }
+}
+
---
+++ Gemlik Patcher/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gemlik_Patcher
+{
+    public partial class GemlikPatcher : Form
+    {
+        public GemlikPatcher()
+        {
+            InitializeComponent();
+            MessageBox.Show("This is an early build, please report any bugs to me.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Settings_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Quit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show( "I'm not mad, just disappointed.", "Do you really want to quit? ", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                MessageBox.Show("BUT " + Environment.UserName + ", I AM YOUR FATHER...'S ACCOUNTANT! \n \n also why didn't you use the quit button, at least make my death ");
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("You just gained additional fun person points!");
+            }
+        }
+    }
+}
